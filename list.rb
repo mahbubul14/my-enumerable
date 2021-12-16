47e2905a577548dfca0036ca118f13fc10d51a0b
mahbubul14 <mahbub21alam2017@gmail.com>
2021-12-17 03:32:43 +0600

    Create all as own emunerable

---
+++ list.rb
@@ -0,0 +1,26 @@
+require_relative './enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*args)
+    @list = args
+  end
+
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts list.all? {|e| e < 5}
+puts list.all? {|e| e > 5}
+
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
+
+puts list.filter {|e| e.even?}
