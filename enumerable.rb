f487c65f78972238eac0cdd6589e995be52d8f5f
Lilian Francis <ifnotlily@yahoo.com>
2021-12-17 09:26:48 +0100

    Update enumerable.rb

--- enumerable.rb
+++ enumerable.rb
@@ -4,4 +4,14 @@ module MyEnumerable
     true
   end
 
+  def any?
+    each {|e| return true if yield e }
+    false
+  end
+  def filter
+    results = []
+    each { |e| results.push(e) if yield e }
+    results
+  end
+  
 end
